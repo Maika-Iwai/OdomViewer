ccb19944bc9351d09ace8560aad8565c8e963c22
Maika-Iwai <160447011@ccalumni.meijo-u.ac.jp>
2019-12-11 20:15:25 +0900

    新規ファイルの追加

---
+++ include/OdomViewer/OdomViewer.h
@@ -0,0 +1,275 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  OdomViewer.h
+ * @brief Odometry Viewer RTC
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef ODOMVIEWER_H
+#define ODOMVIEWER_H
+
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/pcd_io.h>
+
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+#include "TimedPose3DQuaternionStub.h"
+
+// </rtc-template>
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+
+/*!
+ * @class OdomViewer
+ * @brief Odometry Viewer RTC
+ *
+ */
+class OdomViewer
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  /*!
+   * @brief constructor
+   * @param manager Maneger Object
+   */
+  OdomViewer(RTC::Manager* manager);
+
+  /*!
+   * @brief destructor
+   */
+  ~OdomViewer();
+
+  // <rtc-template block="public_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="public_operation">
+  
+  // </rtc-template>
+
+  /***
+   *
+   * The initialize action (on CREATED->ALIVE transition)
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onInitialize();
+
+  /***
+   *
+   * The finalize action (on ALIVE->END transition)
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onFinalize();
+
+  /***
+   *
+   * The startup action when ExecutionContext startup
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The shutdown action when ExecutionContext stop
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The activated action (Active state entry action)
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The deactivated action (Active state exit action)
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The execution action that is invoked periodically
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The aborting action when main logic error occurred.
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The error action in ERROR state
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The reset action that is invoked resetting
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+  
+  /***
+   *
+   * The state update action that is invoked after onExecute() action
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The action that is invoked when execution context's rate is changed
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+
+ protected:
+  // <rtc-template block="protected_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="protected_operation">
+  
+  // </rtc-template>
+
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  /*!
+   * 
+   * - Name:  trajectorybuffer
+   * - DefaultValue: 10000
+   */
+  int m_trajectorybuffer;
+
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  RTC::TimedPose3DQuaternion m_pose3DQuaternion;
+  /*!
+   */
+  RTC::InPort<RTC::TimedPose3DQuaternion> m_pose3DQuaternionIn;
+  
+  // </rtc-template>
+
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+  
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+  
+  // </rtc-template>
+
+ private:
+  // <rtc-template block="private_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="private_operation">
+  
+  // </rtc-template>
+  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr p_cloud;
+  int count;
+  
+};
+
+
+extern "C"
+{
+  DLL_EXPORT void OdomViewerInit(RTC::Manager* manager);
+};
+
+#endif // ODOMVIEWER_H
---
+++ src/OdomViewer.cpp
@@ -0,0 +1,152 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  OdomViewer.cpp
+ * @brief Odometry Viewer RTC
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "OdomViewer.h"
+
+pcl::visualization::CloudViewer viewer("PointCloudViewer");
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* odomviewer_spec[] =
+  {
+    "implementation_id", "OdomViewer",
+    "type_name",         "OdomViewer",
+    "description",       "Odometry Viewer RTC",
+    "version",           "1.0.0",
+    "vendor",            "Maika Iwai, Robot System Design Laboratory, Meijo University",
+    "category",          "ImageProcessing",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.TrajectoryBuffer", "10000",
+
+    // Widget
+    "conf.__widget__.TrajectoryBuffer", "text",
+
+    // Constraints
+
+    "conf.__type__.TrajectoryBuffer", "int",
+
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+OdomViewer::OdomViewer(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_pose3DQuaternionIn("Pose3DQuaternion", m_pose3DQuaternion)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+OdomViewer::~OdomViewer()
+{
+}
+
+
+
+RTC::ReturnCode_t OdomViewer::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("Pose3DQuaternion", m_pose3DQuaternionIn);
+  
+  // Set OutPort buffer
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("TrajectoryBuffer", m_trajectorybuffer, "10000");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t OdomViewer::onActivated(RTC::UniqueId ec_id)
+{
+
+  p_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGBA>);
+  p_cloud->resize(1000);
+  p_cloud->width = m_trajectorybuffer;
+  p_cloud->height = 1;
+  p_cloud->points.resize(p_cloud->width * p_cloud->height);
+  count = 0;
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t OdomViewer::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t OdomViewer::onExecute(RTC::UniqueId ec_id)
+{
+  
+  if(m_pose3DQuaternionIn.isNew()){
+    m_pose3DQuaternionIn.read();
+
+    printf("\r(px, py, pz, tx, ty, tz, tw) = (%5.2f, %5.2f, %5.2f, %5.2f, %5.2f, %5.2f)", m_pose3DQuaternion.point3D.x, m_pose3DQuaternion.point3D.y, m_pose3DQuaternion.point3D.z, m_pose3DQuaternion.quaternion.x, m_pose3DQuaternion.quaternion.y, m_pose3DQuaternion.quaternion.z, m_pose3DQuaternion.quaternion.w);
+    
+    pcl::PointXYZRGBA &point = p_cloud->points[count];
+    point.x = m_pose3DQuaternion.point3D.x;
+    point.y = m_pose3DQuaternion.point3D.y;
+    point.z = m_pose3DQuaternion.point3D.z;
+    point.r = 255;
+    point.g = 255;
+    point.b = 255;
+    point.a = 0;
+    
+    
+    viewer.showCloud(p_cloud);
+    if(count<p_cloud->width){
+      count++;
+    }
+    else{
+      count = 0;
+    }
+  }
+  return RTC::RTC_OK;
+}
+
+extern "C"
+{
+ 
+  void OdomViewerInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(odomviewer_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<OdomViewer>,
+                             RTC::Delete<OdomViewer>);
+  }
+  
+};
+
+
